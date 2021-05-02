369302ae34644c943d69de8451356c74bd6025b6
ruk0sh <ruk0sh@users.noreply.github.com>
2021-05-02 17:46:20 +0300

    initial files and dirs

---
+++ ml_project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Example ML project made at ML in Production Course held at MADE Big Data Academy',
+    author='Ruslan Akhmerov',
+    license='MIT',
+)
