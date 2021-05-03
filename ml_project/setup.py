b3042f528c639d36ece72486dd789d0d6e503ec1
ruk0sh <ruk0sh@users.noreply.github.com>
2021-05-03 17:24:58 +0300

    renamed pipelines to homework1, case: still looks like a bad practice

--- ml_project/setup.py
+++ ml_project/setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='pipelines',
+    name='homework1',
     packages=find_packages(),
     version='0.1.0',
     description='Example ML project made at ML in Production Course held at MADE Big Data Academy',
     author='Ruslan Akhmerov',
     license='MIT',
+    # install_requires=[requirements.txt],
 )
