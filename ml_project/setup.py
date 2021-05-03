9eae75b4e3c949989fa1e008a0c3d3537b93d05d
ruk0sh <ruk0sh@users.noreply.github.com>
2021-05-03 17:04:19 +0300

    renamed src to pipelines, case: bad practice

--- ml_project/setup.py
+++ ml_project/setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='pipelines',
     packages=find_packages(),
     version='0.1.0',
     description='Example ML project made at ML in Production Course held at MADE Big Data Academy',
